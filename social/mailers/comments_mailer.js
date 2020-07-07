0d6bb52f80009480c31d7c3891bbfa30a4305160
Saloni3798 <saloniaggarwal3798@gmail.com>
2020-07-07 14:22:24 +0530

    initial commit

---
+++ social/mailers/comments_mailer.js
@@ -0,0 +1,45 @@
+const nodeMailer = require('../config/nodemailer');
+
+
+// this is another way of exporting a method
+exports.newComment = (comment) => {
+    console.log('inside newComment mailer', comment);
+
+    nodeMailer.transporter.sendMail({
+       from: 'snehaaggarwal26701@gmail.com',
+       to: comment.user.email,
+       subject: "New Comment Published!",
+       html: '<h1>Yup, your comment is now published!</h1>' 
+    }, (err, info) => {
+        if (err){
+            console.log('Error in sending mail', err);
+            return;
+        }
+
+        console.log('Message sent', info);
+        return;
+    });
+}
+// const nodeMailer = require('../config/nodemailer');
+
+
+// // this is another way of exporting a method
+// exports.newComment = (comment) => {
+//     // let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+//     nodeMailer.transporter.sendMail({
+//         from: 'snehaaggarwal26701@gmail.com',
+//        to: comment.user.email,
+//        subject: "New Comment Published!",
+//        html: htmlString
+//     }, (err, info) => {
+//         if (err){
+//             console.log('Error in sending mail', err);
+//             return;
+//         }
+
+//         // console.log('Message sent', info);
+//         return;
+//     });
+// }
+
